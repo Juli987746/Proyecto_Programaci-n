06a30a26e5274f4404a404a7f70ccaeaf225644e
Juli987746 <jmelendez10320@ufide.ac.cr>
2026-04-11 21:49:34 -0600

    Update ProyectoProgra.py
    El modelo de inicio de sesión

--- ProyectoProgra.py
+++ ProyectoProgra.py
@@ -4,6 +4,34 @@ Proyecto final para nuestro curso de progra
 
 """
 
-def mensajeincial
-print("digita el mensaje inicial")
+# Acceso al sistema
+
+usuarios_validos = ["Profesor1", "Profesor2", "Profesor3"]
+password_correcta = "123"
+
+def iniciosesion():
+    intentos = 3
+
+    while intentos > 0:
+        print("Bienvenido, por favor ingresa tus credenciales")
+        user = input("User: ")
+        password = input("Password: ")
+
+        if user in usuarios_validos:
+        if password == password_correcta:
+            print("Acceso concedido")
+            acceso = "si"
+        else:
+            print("Contraseña incorrecta")
+    else:
+        print("Usuario incorrecto")
+      
+    if acceso == "no":
+        intentos = intentos - 1
+        print("Intentos restantes:", intentos)
+    else:
+        break
+
+if acceso == "no":
+  print("Acceso denegado")
 
